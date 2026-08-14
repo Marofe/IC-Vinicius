4d3349ca881d7519c9a9f96d7db408c0c5526a29
vini500300 <59338221+vini500300@users.noreply.github.com>
2026-08-14 18:38:27 -0300

    Revert "Created MATLAB project"
    This reverts commit 851cfd7d354931305d7bcf2cad720397f694400e.

--- main_EuKF_Lie.m
+++ main_EuKF_Lie.m
@@ -136,7 +136,8 @@ title('Gyro bias')
 grid on
 
 %% RMSE
-[rmse_, rmse_ang, rmse_pos, rmse_vel] = evaluateStateRMSE(euler, squeeze(hx(1:3,5,:)), squeeze(hx(1:3,4,:)), ref, Cen)
+[rmse_, rmse_ang, rmse_pos, rmse_vel] = evaluateStateRMSE(euler, squeeze(hx(1:3,5,:)), squeeze(hx(1:3,4,:)), ref, Cen);
+
 %% save
 hx_EuKF_lie = hx;
 % Define output directory relative to where this script lives
